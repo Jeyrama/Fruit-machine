bddb32643505e4cef44dfce3094c4f3480342673
Jey <126204426+Jeyrama@users.noreply.github.com>
2023-12-29 13:04:22 -0800

    Update Fruit-machine.js

--- Fruit-machine.js
+++ Fruit-machine.js
@@ -103,3 +103,19 @@ Scoring:
 
 
 // Solution
+
+function fruit(reels, spins) {
+  let map = ['Jack', 'Queen', 'King', 'Bar', 'Cherry', 'Seven', 'Shell', 'Bell', 'Star', 'Wild'];
+  
+  let [a, b, c] = reels
+    .map((reel, i) => map.indexOf(reel[spins[i]]) + 1)
+    .sort((a, b) => a - b);
+
+  if (a === b && b === c)
+    return a * 10;
+    
+  if (a === b)
+    return c === 10 ? a * 2 : a;
+  
+  return b === c ? b : 0;
+}
