06fd6a5e3d27021f0bf1e93e713f60ce004cbe17
Jey <126204426+Jeyrama@users.noreply.github.com>
2023-12-29 13:21:46 -0800

    Update Fruit-machine.js

--- Fruit-machine.js
+++ Fruit-machine.js
@@ -121,3 +121,45 @@ function fruit(reels, spins) {
 }
 
 // or
+
+function fruit(pizza, spin) {
+	let itemScores = {
+		"Wild":   [100, 10, 0],
+		"Star":   [90,  9, 18],
+		"Bell":   [80,  8, 16],
+		"Shell":  [70,  7, 14],
+		"Seven":  [60,  6, 12],
+		"Cherry": [50,  5, 10],
+		"Bar":    [40,  4,  8],
+		"King":   [30,  3,  6],
+		"Queen":  [20,  2,  4],
+		"Jack":   [10,  1,  2]
+	};
+
+	let shown = [];
+	let wilds = 0;
+
+	for (let i = 0; i < spin.length; i++) {
+		let showing = pizza[i][spin[i]];
+		if (showing === "Wild")
+			wilds++;
+		else 
+			shown.push(showing);
+	}
+
+	if (wilds >= 3)
+		return 100;
+	else if (wilds == 2)
+		return 10;
+
+	if (shown[0] === shown[1] && shown[1] == shown[2])
+		return itemScores[shown[0]][0];
+	else if ((shown[0] == shown[1] && wilds) || (shown[0] == shown[2] && wilds))
+		return itemScores[shown[0]][2];
+	else if (shown[0] == shown[1] || shown[0] == shown[2])
+		return itemScores[shown[0]][1];
+	else if (shown[1] == shown[2])
+		return itemScores[shown[1]][1];
+	else
+		return 0;
+}
